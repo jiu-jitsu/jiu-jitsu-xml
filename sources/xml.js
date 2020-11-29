7d5fef9b39be5ef692d1ddab00f27d2c470ede32
Vitaly Anagorsky <vitaly@anagorsky.com>
2020-11-29 19:18:33 +0300

    0.0.1

--- sources/xml.js
+++ sources/xml.js
@@ -3,6 +3,7 @@
  * 
  */
 
+const util = require("util")
 const childProcess = require("child_process")
 
 /**
@@ -12,5 +13,5 @@ const childProcess = require("child_process")
 module.exports = async (fileXml, fileJson) => {
 	const command = `./xml2json ${fileXml} > ${fileJson}`
 	const options = { cwd: __dirname }
-	await new Promise((resolve) => childProcess.exec(command, options, resolve))
+	await util.promisify(childProcess.exec)(command, options)
 }
