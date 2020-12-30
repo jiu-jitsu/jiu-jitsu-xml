8d17b90f0fdc3655e7c7d55d67ee4417b64804a0
Vitaly Anagorsky <vitaly@anagorsky.com>
2020-12-30 22:48:34 +0300

    0.0.5

--- sources/xml.js
+++ sources/xml.js
@@ -3,17 +3,51 @@
  *
  */
 
-const util = require("util")
 const childProcess = require("child_process")
 
 /**
- *
+ * @param {String} xmlText
+ * @return {Object}
  */
 
-async function xml (fileXml, fileJson) {
-	const command = `./xml2json ${fileXml} > ${fileJson}`
-	const options = { cwd: __dirname }
-	await util.promisify(childProcess.exec)(command, options)
+async function xml (xmlText) {
+
+	/**
+	 *
+	 */
+
+	const buffers = []
+	const xml2json = childProcess.spawn("./xml2json", { cwd: __dirname })
+
+	/**
+	 *
+	 */
+
+	xml2json.stdin.write(xmlText)
+	xml2json.stdin.end()
+
+	/**
+	 *
+	 */
+
+	for await (const buffer of xml2json.stdout) {
+		buffers.push(buffer)
+	}
+
+	/**
+	 *
+	 */
+
+	const jsonBuffer = Buffer.concat(buffers)
+	const jsonString = jsonBuffer.toString()
+	const jsonObject = JSON.parse(jsonString)
+
+	/**
+	 *
+	 */
+
+	return jsonObject
+
 }
 
 /**
